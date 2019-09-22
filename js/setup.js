8a416835e3da56843c4fbc5e880fa631cd3da921
Valeriya <roslavcevavaleriya1@bk.ru>
2019-09-22 21:06:09 +0300

    Задание "Нас орда"

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
 var COUNT_WIZARD = 4;
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)','rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
@@ -10,7 +10,7 @@ userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 
-var getRandomElement = function(array) {
+var getRandomElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
@@ -24,7 +24,7 @@ var createWizard = function (name, surname, coatColor, eyesColor) {
   return wizard;
 };
 
-var createWizardArray = function() {
+var createWizardArray = function () {
   var wizards = [];
   for (var i = 0; i < COUNT_WIZARD; i++) {
     wizards.push(createWizard(NAMES, SURNAMES, COAT_COLOR, EYES_COLOR));
@@ -38,11 +38,11 @@ var wizards = createWizardArray();
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-for(var i = 0; i < COUNT_WIZARD; i++) {
+for (var i = 0; i < COUNT_WIZARD; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
   similarListElement.appendChild(wizardElement);
-};
+}
